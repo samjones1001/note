4419b933c1a9ddf19f2349ed01808453451a7c1b
Sam Jones <sam.jones494@googlemail.com>
2017-02-13 15:29:10 +0000

    challenge 2

---
+++ spec/note-list-tests.js
@@ -0,0 +1,15 @@
+(function() {
+	assert.isTrue(notelist = new NoteList());
+})();
+
+(function() {
+	notelist = new NoteList();
+	assert.isEmpty(notelist.getNotes());
+})();
+
+(function() {
+	notelist = new NoteList();
+	notelist.addNote("this is a note");
+	assert.isNotEmpty(notelist.getNotes());
+})();
+
