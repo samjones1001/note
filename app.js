1cf6b2dbc57ef9738922815f551a0484a76d6e90
Sam Jones <sam.jones494@googlemail.com>
2017-02-13 17:35:29 +0000

    set up http-server

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("howdy");
+var httpServer = require("http-server");
+var path = require("path");
+
+var pathToHtmlAndJsFiles = path.join(__dirname, "./src");
+var server = httpServer.createServer({ root: pathToHtmlAndJsFiles });
+server.listen(3000);
