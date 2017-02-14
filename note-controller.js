3fa895dfc010cbacf6f44d21a7a95bff3c421ecb
Justinio14 <jbutwell@hotmail.com>
2017-02-14 16:45:56 +0000

    WIP ex6

--- note-controller.js
+++ note-controller.js
@@ -1,6 +1,18 @@
+(function(exports) {
 
 
-  (function appHtml() {
-  var elem = document.getElementById('app');
-    document.body.innerHTML = "Howdy";
-  })();
+
+  function NoteController(newlist = new NoteList()) {
+    this._newList = newlist;
+    this._newListed = this._newList.AddNote("Iron Bru");
+    this._view = new NoteView(newlist);
+  }
+
+  NoteController.prototype.WebPost = function() {
+    var elem = document.getElementById('app');
+      document.body.innerHTML = this._view.display();
+  }
+
+  exports.NoteController = NoteController;
+
+})(this);
