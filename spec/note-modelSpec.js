cb126afcdc89f65f95bb16dcffc5974a17c657e9
Sam Jones <sam.jones494@googlemail.com>
2017-02-15 12:37:37 +0000

    started challenge 9

--- spec/note-modelSpec.js
+++ spec/note-modelSpec.js
@@ -7,3 +7,14 @@ function testingNewDisplay(string){
 }
 
 testingNewDisplay("book");
+
+function eachNoteHasAnId(){
+	note = new Note("Note 1")
+	note2 = new Note ("Note 2")
+
+	if (note2.id !== 2) {
+		throw new Error("Each note shoud have a unique ID")
+	}
+}
+
+eachNoteHasAnId();
--- src/note-model.js
+++ src/note-model.js
@@ -2,8 +2,12 @@
 
 (function(exports){
 
+  var NOTE_ID_COUNT = 0;
+
   function Note(text){
   this.text = text;
+  this.id = NOTE_ID_COUNT;
+  NOTE_ID_COUNT ++;
   }
 
   function displayNote (){
