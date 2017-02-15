f7c282b824eba21c6e12f81fe4089d40c90f0c15
Sam Jones <sam.jones494@googlemail.com>
2017-02-15 12:20:39 +0000

    Challenge 8 complete

--- spec/note-list-viewSpec.js
+++ spec/note-list-viewSpec.js
@@ -4,9 +4,21 @@ function testingNoteListview(){
   notelist.createNote("Food");
   notelistview = new NoteListView(notelist);
 
-  if (notelistview.returnHTMLString() !== '<ul><li><div>Book</div></li><div><ul><li><div>Food</div></li><div>') {
+  if (notelistview.returnHTMLString() !== '<ul><li><div>Book</div></li><li><div>Food</div></li></ul>') {
     throw new Error('It should return Book and Food');
   }
 }
 
 testingNoteListview();
+
+function testFormattingOfLongNotes(){
+	notelist = new NoteList();
+	notelist.createNote("This is a very very very long note");
+	notelistview = new NoteListView(notelist);
+
+	if (notelistview.returnHTMLString() !== '<ul><li><div>This is a very very ...</div></li></ul>') {
+		throw new Error('It should only display the first 20 characters of a note');
+	}
+}
+
+testFormattingOfLongNotes()
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -8,12 +8,18 @@
 function returnHTMLString()  {
   var string = "";
   for (var i = 0; i < this.view.length; i++){
-    string += '<ul><li><div>' + this.view[i].displayNote() + '</div></li><div>'
+  	if (this.view[i].displayNote().length > 20) {
+  		string += '<li><div>' + this.view[i].displayNote().substring(0, 20) + '...</div></li>'
+  	} else {
+  		string += '<li><div>' + this.view[i].displayNote()+ '</div></li>'
+  	}    
   }
-return string;
+return "<ul>" + string + "</ul>";
 }
 
 exports.NoteListView = NoteListView;
 NoteListView.prototype.returnHTMLString = returnHTMLString;
 
 }(this));
+
+
